ea506334e47a22078edafb2bf189d7e5dd0e2971
vitaliyz <vitaliy.zadorozhniy.od@gmail.com>
2020-01-17 22:51:16 +0200

    start

---
+++ checkio.py
@@ -0,0 +1,63 @@
+"""In this mission you should check if all elements in the given list are equal.
+Input: List.
+Output: Bool."""
+
+
+def elements_equals(some_list: list) -> bool:
+    if some_list:
+        return True if some_list.count(some_list[0]) == len(some_list) else False
+    return True
+
+
+"""The password will be considered strong enough if its length is 
+greater than or equal to 10 symbols, it has at least one digit, as well as containing one uppercase letter and one 
+lowercase letter in it. The password contains only ASCII latin letters or digits.
+Input: A password as a string.
+Output: Is the password safe or not as a boolean or any data type that can be converted and processed as a boolean."""
+
+
+def secure_password(data: str) -> bool:
+    import re
+    return True if len(data) >= 10 and re.search("[a-z]+", data) and re.search("[A-Z]+", data) and re.search("[0-9]+", data) else False
+
+
+"""You are given a sequence of strings. You should join these strings into chunk of text where the initial strings are
+separated by commas. You should replace all cases of the words "right" with the word "left", even if it's a part of 
+another word. All strings are given in lowercase.
+Input: A sequence of strings as a tuple of strings (unicode).
+Output: The text as a string."""
+
+
+def left_join(phrases: tuple) -> str:
+    return ','.join([i.replace('right', 'left') for i in phrases])
+
+
+"""You are given a string with words and numbers separated by whitespaces (one space). The words contains only letters. 
+You should check if the string contains three words in succession. For example, the string "start 5 one two three 7 end"
+contains three words in succession.
+Input: A string with words.
+Output: The answer as a boolean."""
+
+
+def words_count(words: str) -> bool:
+    res = words.split()
+    count = 0
+    for i in res:
+        if i.isalpha():
+            count += 1
+        else:
+            if count < 3:
+                count = 0
+    return True if count >= 3 else False
+
+
+"""Find the TOP most expensive goods. The amount we are looking for will be given as a first argument and the whole
+data as the second one
+Input: int and list of dicts. Each dicts has two keys "name" and "price"
+Output: the same as the second Input argument."""
+
+
+def bigger_price(limit: int, data: list) -> list:
+    return list(reversed(sorted(data, key=lambda x: x.get('price'))[-limit:]))
+
+
